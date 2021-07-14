40747d384866dc44216ed0e42dcc04b5287c9646
Pablo Gómez <79404935+Pablog21@users.noreply.github.com>
2021-07-14 20:48:47 -0300

    Update RecoSystem.py

--- RecoSystem.py
+++ RecoSystem.py
@@ -1,3 +1,4 @@
+#First we will donwload the dataset from IBM CLOUD
 !wget -O moviedataset.zip https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%205/data/moviedataset.zip
 print('unziping ...')
 !unzip -o -j moviedataset.zip 
